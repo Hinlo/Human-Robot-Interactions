442da556b06402c6d4903fce491ba82eeee2f412
Hinlo <57256469+Hinlo@users.noreply.github.com>
2023-05-08 15:40:04 +0100

    Add files via upload

---
+++ Choregraphe_Flows/NaoYogaUninteractive/translations/translation_en_US.ts
@@ -0,0 +1,237 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Choice (1)/Choice</name>
+        <message>
+            <source>no/no nao/disagree/I disagree</source>
+            <comment>SpeechReco negative</comment>
+            <translation type="obsolete">no/no nao/disagree/I disagree</translation>
+        </message>
+        <message>
+            <source>yes/yes nao/agree/I agree/of course</source>
+            <comment>SpeechReco positive</comment>
+            <translation type="obsolete">yes/yes nao/agree/I agree/of course</translation>
+        </message>
+        <message>
+            <source>help/help me/i don't know</source>
+            <comment>SpeechReco help</comment>
+            <translation type="obsolete">help/help me/i don't know</translation>
+        </message>
+        <message>
+            <source>exit/stop/quit</source>
+            <comment>SpeechReco exit</comment>
+            <translation type="obsolete">exit/stop/quit</translation>
+        </message>
+        <message>
+            <source>repeat/pardon/what/excuse me</source>
+            <comment>SpeechReco repeat</comment>
+            <translation type="obsolete">repeat/pardon/what/excuse me</translation>
+        </message>
+        <message>
+            <source>I understood %s. Is that correct?</source>
+            <comment>TTS confirmation</comment>
+            <translation type="obsolete">I understood %s. Is that correct?</translation>
+        </message>
+        <message>
+            <source>, / or </source>
+            <comment>TTS enumMarks</comment>
+            <translation type="obsolete">, / or </translation>
+        </message>
+        <message>
+            <source>No answer is really expected. /Your answer can be %s. /Your answer can be for example %s. /%s?</source>
+            <comment>TTS helpEnumChoices</comment>
+            <translation type="obsolete">No answer is really expected. /Your answer can be %s. /Your answer can be for example %s. /%s?</translation>
+        </message>
+        <message>
+            <source> you can ask me: %s. </source>
+            <comment>TTS helpEnumDefault</comment>
+            <translation type="obsolete"> you can ask me: %s. </translation>
+        </message>
+        <message>
+            <source>You can also use my tactile sensor to choose the answer./\Pau=600\ Press the front or the rear of my skull cap to go through all of the available answers. Press then the circle in the middle to validate your choice.</source>
+            <comment>TTS helpTactile</comment>
+            <translation type="obsolete">You can also use my tactile sensor to choose the answer./\Pau=600\ Press the front or the rear of my skull cap to go through all of the available answers. Press then the circle in the middle to validate your choice.</translation>
+        </message>
+        <message>
+            <source>I did not understand. </source>
+            <comment>TTS notUnderstood</comment>
+            <translation type="obsolete">I did not understand. </translation>
+        </message>
+        <message>
+            <source>No question has been defined, so I cannot repeat it. </source>
+            <comment>TTS noQuestion</comment>
+            <translation type="obsolete">No question has been defined, so I cannot repeat it. </translation>
+        </message>
+        <message>
+            <source>There is too much noise here! /Try to talk to me more distinctly. /I can't hear you very well. </source>
+            <comment>TTS notUnderstoodAnims</comment>
+            <translation type="obsolete">There is too much noise here! /Try to talk to me more distinctly. /I can't hear you very well. </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Choice (2)/Choice</name>
+        <message>
+            <source>no/no nao/disagree/I disagree</source>
+            <comment>SpeechReco negative</comment>
+            <translation type="obsolete">no/no nao/disagree/I disagree</translation>
+        </message>
+        <message>
+            <source>yes/yes nao/agree/I agree/of course</source>
+            <comment>SpeechReco positive</comment>
+            <translation type="obsolete">yes/yes nao/agree/I agree/of course</translation>
+        </message>
+        <message>
+            <source>help/help me/i don't know</source>
+            <comment>SpeechReco help</comment>
+            <translation type="obsolete">help/help me/i don't know</translation>
+        </message>
+        <message>
+            <source>exit/stop/quit</source>
+            <comment>SpeechReco exit</comment>
+            <translation type="obsolete">exit/stop/quit</translation>
+        </message>
+        <message>
+            <source>repeat/pardon/what/excuse me</source>
+            <comment>SpeechReco repeat</comment>
+            <translation type="obsolete">repeat/pardon/what/excuse me</translation>
+        </message>
+        <message>
+            <source>I understood %s. Is that correct?</source>
+            <comment>TTS confirmation</comment>
+            <translation type="obsolete">I understood %s. Is that correct?</translation>
+        </message>
+        <message>
+            <source>, / or </source>
+            <comment>TTS enumMarks</comment>
+            <translation type="obsolete">, / or </translation>
+        </message>
+        <message>
+            <source>No answer is really expected. /Your answer can be %s. /Your answer can be for example %s. /%s?</source>
+            <comment>TTS helpEnumChoices</comment>
+            <translation type="obsolete">No answer is really expected. /Your answer can be %s. /Your answer can be for example %s. /%s?</translation>
+        </message>
+        <message>
+            <source> you can ask me: %s. </source>
+            <comment>TTS helpEnumDefault</comment>
+            <translation type="obsolete"> you can ask me: %s. </translation>
+        </message>
+        <message>
+            <source>You can also use my tactile sensor to choose the answer./\Pau=600\ Press the front or the rear of my skull cap to go through all of the available answers. Press then the circle in the middle to validate your choice.</source>
+            <comment>TTS helpTactile</comment>
+            <translation type="obsolete">You can also use my tactile sensor to choose the answer./\Pau=600\ Press the front or the rear of my skull cap to go through all of the available answers. Press then the circle in the middle to validate your choice.</translation>
+        </message>
+        <message>
+            <source>I did not understand. </source>
+            <comment>TTS notUnderstood</comment>
+            <translation type="obsolete">I did not understand. </translation>
+        </message>
+        <message>
+            <source>No question has been defined, so I cannot repeat it. </source>
+            <comment>TTS noQuestion</comment>
+            <translation type="obsolete">No question has been defined, so I cannot repeat it. </translation>
+        </message>
+        <message>
+            <source>There is too much noise here! /Try to talk to me more distinctly. /I can't hear you very well. </source>
+            <comment>TTS notUnderstoodAnims</comment>
+            <translation type="obsolete">There is too much noise here! /Try to talk to me more distinctly. /I can't hear you very well. </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Choice/Choice</name>
+        <message>
+            <source>no/no nao/disagree/I disagree</source>
+            <comment>SpeechReco negative</comment>
+            <translation type="obsolete">no/no nao/disagree/I disagree</translation>
+        </message>
+        <message>
+            <source>yes/yes nao/agree/I agree/of course</source>
+            <comment>SpeechReco positive</comment>
+            <translation type="obsolete">yes/yes nao/agree/I agree/of course</translation>
+        </message>
+        <message>
+            <source>help/help me/i don't know</source>
+            <comment>SpeechReco help</comment>
+            <translation type="obsolete">help/help me/i don't know</translation>
+        </message>
+        <message>
+            <source>exit/stop/quit</source>
+            <comment>SpeechReco exit</comment>
+            <translation type="obsolete">exit/stop/quit</translation>
+        </message>
+        <message>
+            <source>repeat/pardon/what/excuse me</source>
+            <comment>SpeechReco repeat</comment>
+            <translation type="obsolete">repeat/pardon/what/excuse me</translation>
+        </message>
+        <message>
+            <source>I understood %s. Is that correct?</source>
+            <comment>TTS confirmation</comment>
+            <translation type="obsolete">I understood %s. Is that correct?</translation>
+        </message>
+        <message>
+            <source>, / or </source>
+            <comment>TTS enumMarks</comment>
+            <translation type="obsolete">, / or </translation>
+        </message>
+        <message>
+            <source>No answer is really expected. /Your answer can be %s. /Your answer can be for example %s. /%s?</source>
+            <comment>TTS helpEnumChoices</comment>
+            <translation type="obsolete">No answer is really expected. /Your answer can be %s. /Your answer can be for example %s. /%s?</translation>
+        </message>
+        <message>
+            <source> you can ask me: %s. </source>
+            <comment>TTS helpEnumDefault</comment>
+            <translation type="obsolete"> you can ask me: %s. </translation>
+        </message>
+        <message>
+            <source>You can also use my tactile sensor to choose the answer./\Pau=600\ Press the front or the rear of my skull cap to go through all of the available answers. Press then the circle in the middle to validate your choice.</source>
+            <comment>TTS helpTactile</comment>
+            <translation type="obsolete">You can also use my tactile sensor to choose the answer./\Pau=600\ Press the front or the rear of my skull cap to go through all of the available answers. Press then the circle in the middle to validate your choice.</translation>
+        </message>
+        <message>
+            <source>I did not understand. </source>
+            <comment>TTS notUnderstood</comment>
+            <translation type="obsolete">I did not understand. </translation>
+        </message>
+        <message>
+            <source>No question has been defined, so I cannot repeat it. </source>
+            <comment>TTS noQuestion</comment>
+            <translation type="obsolete">No question has been defined, so I cannot repeat it. </translation>
+        </message>
+        <message>
+            <source>There is too much noise here! /Try to talk to me more distinctly. /I can't hear you very well. </source>
+            <comment>TTS notUnderstoodAnims</comment>
+            <translation type="obsolete">There is too much noise here! /Try to talk to me more distinctly. /I can't hear you very well. </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Choice/Say</name>
+        <message>
+            <source>Please copy my pose.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Please copy my pose.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Choice/Say (1)</name>
+        <message>
+            <source>okay, let's skip this one. </source>
+            <comment>Text</comment>
+            <translation type="obsolete">okay, let's skip this one. </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Choice/Say (2)</name>
+        <message>
+            <source>okay, let's hold this pose for ten seconds!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">okay, let's hold this pose for ten seconds!</translation>
+        </message>
+        <message>
+            <source>let's hold this pose for ten seconds!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">let's hold this pose for ten seconds!</translation>
+        </message>
+    </context>
+</TS>
